2e1f094ced3d5ca2640d3d52aa1d4d5ef25afe2a
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2023-02-23 20:26:16 +0100

    Test (& abandon) Spling

--- app/src/views/social/index.tsx
+++ app/src/views/social/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useMemo, useState } from "react";
+import { FC, useCallback, useEffect, useMemo, useState } from "react";
 import {
   Keypair,
   LAMPORTS_PER_SOL,
@@ -9,6 +9,16 @@ import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { notify } from "../../utils/notifications";
 import { AnchorProvider, BN, Idl, Program } from "@coral-xyz/anchor";
 import dataAccountSecretKey from "../../dataAccountSecretKey.json";
+import { ProtocolOptions, SocialProtocol, User } from "@spling/social-protocol";
+
+/*
+
+tmcyrix | Spling Labs — Today at 4:09 PM
+Hey! Right, you cant test the sdk while you are moving on devnet. If you dont specify a payer wallet which means that the end users have to pay for the transaction costs then they need just little SOL and SHDW in there wallet to pay for the transaction costs.
+
+Edit: The content will be stored on the user's storage account, which is not possible on devnet thats the reason why you cant test the sdk on devnet first. 
+
+*/
 
 const programId = new PublicKey(process.env.NEXT_PUBLIC_PROGRAM_ID);
 console.log("programId:", programId);
@@ -16,6 +26,11 @@ console.log("programId:", programId);
 // const generatedDataAccount = Keypair.generate();
 const dataAccount = Keypair.fromSecretKey(new Uint8Array(dataAccountSecretKey));
 console.log("dataAccount:", dataAccount);
+
+const options = {
+  rpcUrl: "https://api.mainnet-beta.solana.com/",
+  useIndexer: true,
+} as ProtocolOptions;
 
 export const SocialView: FC = ({}) => {
   const [dataAccountInitialized, setDataAccountInitialized] = useState(!false);
@@ -29,6 +44,16 @@ export const SocialView: FC = ({}) => {
     [connection, wallet]
   );
   const { publicKey: user, sendTransaction } = wallet;
+  const [socialProtocol, setSocialProtocol] = useState<SocialProtocol>();
+
+  async function signUp() {
+    const user: User = await socialProtocol.createUser(
+      "Grizzly",
+      null,
+      "A user account created for testing purposes.",
+      { interests: ["Solana"] }
+    );
+  }
 
   useEffect(() => {
     // fetchIdl();
@@ -53,12 +78,36 @@ export const SocialView: FC = ({}) => {
     // setProgram(program);
   }, [idl, provider]);
 
+  useEffect(() => {
+    async function initSocialProtocol() {
+      try {
+        const socialProtocol: SocialProtocol = await new SocialProtocol(
+          wallet,
+          null,
+          options
+        ).init();
+        console.log("initSocialProtocol: socialProtocol is", socialProtocol);
+        setSocialProtocol(socialProtocol);
+        // await socialProtocol.prepareWallet()
+      } catch {}
+    }
+
+    console.log("wallet:", wallet);
+    initSocialProtocol();
+  }, [wallet]);
+
   return (
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10 mb-8">
           Social
         </h1>
+        <button
+          className={`bg-gradient-to-br from-indigo-500 to-fuchsia-500 p-1 rounded-sm self-stretch`}
+          onClick={signUp}
+        >
+          Sign up
+        </button>
       </div>
     </div>
   );
